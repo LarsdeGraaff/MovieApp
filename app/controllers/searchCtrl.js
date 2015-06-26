c95fcd53a5497771cbe31d9287abf5726242898b
LarsdeGraaff <lars.dg@gmail.com>
2015-06-26 14:08:13 +0200

    added $http function and $log

--- app/controllers/searchCtrl.js
+++ app/controllers/searchCtrl.js
@@ -3,8 +3,19 @@
  */
 
 
-function searchCtrl($scope){
+function searchCtrl($scope,$http,apiUrl,$log){
     $scope.message="Search form"
+    $scope.searchMovie=function(title){
+        $log.debug(title);
+
+        var url=apiUrl + 'movies/search?title=' + title;
+        $http.get(url).success(function(data) {
+            $log.debug(data);
+        });
+
+        $log.debug('run');
+
+    }
 }
 
 angular.module('movieApp.controllers')
